dbc3715b6b2603f6ce529b0896891204332a92fe
Rodrigo Zamora <zamorarodrigo96@gmail.com>
2025-05-13 22:38:24 -0300

    Implementacion de clase Grafo para objetos, agregando Arista y Vertice como tales

---
+++ src/grafo/Arista.java
@@ -0,0 +1,14 @@
+package grafo;
+
+public class Arista {
+    private String peso;
+
+
+    public String getPeso() {
+        return peso;
+    }
+
+    public void setPeso(String peso) {
+        this.peso = peso;
+    }
+}
--- src/grafo/Grafo.java
+++ src/grafo/Grafo.java
@@ -4,8 +4,8 @@ import lista.ILista;
 import lista.Lista;
 
 public class Grafo {
-    private int[][] aristas;
-    private boolean[] vertices;
+    private Arista[][] aristas;
+    private Vertice[] vertices;
     private int cantidad;
     private final int maxVertices;
     private final boolean dirigido;
@@ -13,15 +13,34 @@ public class Grafo {
 
     public Grafo(int cantMaxVertices, boolean esDirigido) {
         cantidad = 0;
-        vertices = new boolean[cantMaxVertices + 1];
-        aristas = new int[cantMaxVertices + 1][cantMaxVertices + 1];
+        vertices = new Vertice[cantMaxVertices];
+        aristas = new Arista[cantMaxVertices][cantMaxVertices];
+        if (esDirigido) {
+            for (int i = 0; i < aristas.length; i++) {
+                for (int j = 0; j < aristas.length; j++) {
+                    aristas[i][j] = new Arista();
+                }
+            }
+        } else {
+            for (int i = 0; i < aristas.length; i++) {
+                for (int j = i; j < aristas.length; j++) {
+                    Arista arista = new Arista();
+                    aristas[i][j] = arista;
+                    aristas[j][i] = arista; // Asignar la misma arista en ambas direcciones
+                }
+            }
+        }
         maxVertices = cantMaxVertices;
         dirigido = esDirigido;
     }
 
-    public void agregarVertice(int vertice) {
-        vertices[vertice] = true;
-        cantidad++;
+    public void agregarVertice(Vertice vertice) {
+
+        if (cantidad < maxVertices) {
+            int posLibre = obtenerPosLibre();
+            vertices[posLibre] = vertice;
+            cantidad++;
+        }
     }
 
     public void borrarVertice(int vertice) {
@@ -34,31 +53,55 @@ public class Grafo {
         cantidad--;
     }
 
-    public void agregarArista(int vInicio, int vFinal, int peso) {
-        aristas[vInicio][vFinal] = peso;
-        if (!dirigido) {
-            aristas[vFinal][vInicio] = peso;
-        }
+    public void agregarArista(Vertice vInicio, Vertice vFinal, Arista arista) {
+        int posVinicial = obtenerPos(vInicio);
+        int posVfinal = obtenerPos(vFinal);
+        Arista a = aristas[posVinicial][posVfinal];
+        a.setPeso(arista.getPeso());
+
     }
 
-    public void borrarArista(int vInicio, int vFinal){
+
+
+    public void borrarArista(int vInicio, int vFinal) {
         aristas[vInicio][vFinal] = 0;
-        if(!dirigido){
+        if (!dirigido) {
             aristas[vFinal][vInicio] = 0;
         }
     }
 
-    public int obtenerArista(int vInicio, int vFinal){
+    public int obtenerArista(int vInicio, int vFinal) {
         return aristas[vInicio][vFinal];
     }
 
-    public ILista<Integer> adyacentes(int vertice){
+    public ILista<Integer> adyacentes(int vertice) {
         ILista<Integer> adyacentes = new Lista<>();
-        for(int i = 1; i < aristas.length; i++){
-            if(aristas[vertice][i] > 0){
+        for (int i = 1; i < aristas.length; i++) {
+            if (aristas[vertice][i] > 0) {
                 adyacentes.insertar(i);
             }
         }
         return adyacentes;
+    }
+
+
+    //Metodos privados
+
+    private int obtenerPosLibre() {
+        for (int i = 0; i < vertices.length; i++) {
+            if (vertices[i] == null) {
+                return i;
+            }
+        }
+        return -1; // No hay espacio libre
+    }
+
+    private int obtenerPos(Vertice v) {
+        for (int i = 0; i < vertices.length; i++) {
+            if (vertices[i] != null && vertices[i].equals(v)) {
+                return i;
+            }
+        }
+        return -1; // No se encontró el vértice
     }
 }
